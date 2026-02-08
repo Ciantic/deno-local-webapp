57972fbd1eaedd575d73fb67bed2dd336cff9c49
Jari O. O. Pennanen <ciantic@oksidi.com>
2026-02-09 00:33:13 +0200

    ...

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -1,6 +1,6 @@
 /* @refresh reload */
-import "./ui/index.css";
+import "./index.css";
 import { render } from "solid-js/web";
-import App from "./ui/App.tsx";
+import App from "./App.tsx";
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
