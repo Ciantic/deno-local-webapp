6f03db6755fb3cd5496da358b96e68a13d961485
Jari O. O. Pennanen <ciantic@oksidi.com>
2026-03-07 18:35:14 +0200

    Solidjs 2 beta

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -1,6 +1,6 @@
 /* @refresh reload */
 import "./index.css";
-import { render } from "solid-js/web";
+import { render } from "@solidjs/web";
 import App from "./App.tsx";
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
